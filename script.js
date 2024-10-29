13e236e47c676ee4df3c716dfc0fa9434b4029f7
Biaaannnn <atlassavianalsaki@gmail.com>
2024-10-29 20:31:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript untuk logika kalkulator
+function appendDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+// Modifikasi fungsi calculateResult agar hasilnya selalu "hi pretty girl"
+function calculateResult() {
+    document.getElementById("display").value = "hi pretty girl";
+}
