944fc4f9106d7ac7414b86917201fae862af70b8
Biaaannnn <atlassavianalsaki@gmail.com>
2024-10-30 11:59:12 +0700

    anim.js
    Signed-off-by: Biaaannnn <atlassavianalsaki@gmail.com>

--- anim.js
+++ anim.js
@@ -4,33 +4,20 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "So I'ma love you every night", time: 1 },
-  { text: "The whisper of birds", time: 18 },
-  { text: "Lonely before the sun cried", time: 27 },
-  { text: "Fell from the sky", time: 32 },
-  { text: "Like water drops", time: 33 },
-  { text: "Where I'm now? I don't know why", time: 41 },
-  { text: "Nice butterflies in my hands", time: 47 },
-  { text: "Too much light for twilight", time: 54 },
-  { text: "In the mood for the flowers love", time: 59 },
-  { text: "That vision", time: 67 },
-  { text: "Really strong, blew my mind", time: 72 },
-  { text: "Silence Let me see what it was", time: 78 },
-  { text: "I only want to live in clouds", time: 83 },
-  { text: "Where I'm now? I don't know why", time: 91 },
-  { text: "Nice butterflies in my hands", time: 97 },
-  { text: "Too much light for twilight", time: 104 },
-  { text: "In the mood for the flowers love", time: 108 },
-  { text: "At the time", time: 144 },
-  { text: "The whisper of birds", time: 148 },
-  { text: "Lonely before the sun cried", time: 153 },
-  { text: "Fell from the sky", time: 158 },
-  { text: "Like water drops", time: 164 },
-  { text: "Where I'm now? I don't know why", time: 169 },
-  { text: "Nice butterflies in my hands", time: 176 },
-  { text: "Too much light for twilight", time: 183 },
-  { text: "In the mood for the flowers", time: 188 },
-  { text: "Love.", time: 140 },
+  { time: 0, text: "Like it's the last night" },
+  { time: 3, text: "If the world was ending" },
+  { time: 5, text: "I'd wanna be next" },
+  { time: 8.5, text: "to you" },
+  { time: 11, text: "If the party" },
+  { time: 12, text: "was over" },
+  { time: 15, text: "And our time on Earth" },
+  { time: 18, text: " was through" },
+  { time: 20, text: "I'd wanna hold you" },
+  { time: 23, text: "just for a while" },
+  { time: 28, text: "And die with a smile" },
+  { time: 31, text: " If the world was ending" },
+  { time: 33, text: " I'd wanna be next" },
+  { time: 35, text: " to you" },
 ];
 
 // Animar las letras
@@ -70,3 +57,4 @@ function ocultarTitulo() {
 
 // Llama a la función después de 216 segundos (216,000 milisegundos)
 setTimeout(ocultarTitulo, 216000);
+
